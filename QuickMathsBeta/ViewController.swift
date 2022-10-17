5a93b0be58f9857200674f28d8502a83c73a7a9e
Peter Ryan <peter.luke.ryan@gmail.com>
2022-10-17 15:42:18 -0500

    Initial Commit

---
+++ QuickMathsBeta/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuickMathsBeta
+//
+//  Created by Peter Ryan on 10/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
