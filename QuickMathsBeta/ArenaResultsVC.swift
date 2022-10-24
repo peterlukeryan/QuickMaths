f1dbe497b34b3b5bc64b51ac9911b8b2a0c3314e
Peter Ryan <peter.luke.ryan@gmail.com>
2022-10-24 15:16:20 -0500

    Added View Controller files

---
+++ QuickMathsBeta/ArenaResultsVC.swift
@@ -0,0 +1,29 @@
+//
+//  ArenaResultsVC.swift
+//  QuickMathsBeta
+//
+//  Created by Peter Ryan on 10/24/22.
+//
+
+import UIKit
+
+class ArenaResultsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ QuickMathsBeta/ArenaVC.swift
@@ -0,0 +1,29 @@
+//
+//  ArenaVC.swift
+//  QuickMathsBeta
+//
+//  Created by Peter Ryan on 10/24/22.
+//
+
+import UIKit
+
+class ArenaVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
