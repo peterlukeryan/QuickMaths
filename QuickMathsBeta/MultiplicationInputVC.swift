f4be42f8ffa49865c8458049d387e0de8240289e
Vanessa Z <vanlouisezam@gmail.com>
2022-10-26 13:30:00 -0500

    Added basic logic for running sums and multiplication demo, as well as segue data transfer

--- QuickMathsBeta/MultiplicationInputVC.swift
+++ QuickMathsBeta/MultiplicationInputVC.swift
@@ -2,40 +2,58 @@
 //  MultiplicationInputVC.swift
 //  QuickMathsBeta
 //
-//  Created by Peter Ryan on 10/24/22.
+//  Created by Vanessa Zambrano on 10/24/22.
 //
 
 import UIKit
 
 class MultiplicationInputVC: UIViewController {
 
-    @IBOutlet weak var firstNumber: UITextField!
+    @IBOutlet weak var firstNumberField: UITextField!
     
-    @IBOutlet weak var secondNumber: UITextField!
+    @IBOutlet weak var secondNumberField: UITextField!
+    
+    var firstNumber = 0.0
+    var secondNumber = 0.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
     @IBAction func showBtn(_ sender: Any) {
         // perform input validation -- length of input must be less than 99
-        if (Int(firstNumber.text!)! > 99 || Int(firstNumber.text!)! > 99) {
-            // make error pop up asking user to enter two 2-digit numbers
+        if ((Int(firstNumberField.text!) ?? 100) > 99 || (Int(firstNumberField.text!) ?? 100) > 99) {
+            let alert = UIAlertController(title: "Invalid inputs!", message: "Please enter a number in each field that is less than 99.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: .cancel, handler: { _ in
+                self.dismiss(animated: true, completion: nil)
+            }))
+            self.present(alert, animated: true, completion: nil)
         }
-        // else allow segue
-        // transfer number data to the other screen -- probably using prepare?
+        // perform input validation -- at least one number must be 2 digits long
+        else if ((Int(firstNumberField.text!) ?? 0) + (Int(firstNumberField.text!) ?? 0) <= 18) {
+            let alert = UIAlertController(title: "Invalid inputs!", message: "One number must be at least 2 digits long!", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: .cancel, handler: { _ in
+                self.dismiss(animated: true, completion: nil)
+            }))
+            self.present(alert, animated: true, completion: nil)
+        }
+        else {
+            firstNumber = Double(firstNumberField.text!)!
+            secondNumber = Double(secondNumberField.text!)!
+            
+            performSegue(withIdentifier: "multi_seg", sender: self)
+        }
     }
-
-    /*
-     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "multi_seg" {
+            let multiDemo = segue.destination as! MultiplicationVisualVC
+            // Pass the selected object to the new view controller.
+            multiDemo.mainMultiplier = firstNumber
+            multiDemo.mainMultiplicand = secondNumber
+        }
     }
-    */
 
 }
--- QuickMathsBeta/MultiplicationVisualVC.swift
+++ QuickMathsBeta/MultiplicationVisualVC.swift
@@ -2,17 +2,88 @@
 //  MultiplicationVisualVC.swift
 //  QuickMathsBeta
 //
-//  Created by Peter Ryan on 10/24/22.
+//  Created by Vanessa Zambrano on 10/24/22.
 //
 
 import UIKit
 
 class MultiplicationVisualVC: UIViewController {
-
+    
+    var mainMultiplier = 0.0
+    var mainMultiplicand = 0.0
+    
+    @IBOutlet weak var mainExpression: UILabel!
+    @IBOutlet weak var subExp1: UILabel!
+    @IBOutlet weak var subExp2: UILabel!
+    @IBOutlet weak var subExp3: UILabel!
+    @IBOutlet weak var subExp4: UILabel!
+    
+    @IBOutlet weak var runningSum1Lbl: UILabel!
+    @IBOutlet weak var runningSum2Lbl: UILabel!
+    @IBOutlet weak var runningSum3Lbl: UILabel!
+    @IBOutlet weak var runningSum4Lbl: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let formatter = NumberFormatter()
+        formatter.minimumFractionDigits = 0
 
-        // Do any additional setup after loading the view.
+        mainExpression.text = String(format: "%.0f", mainMultiplier) + " X " + String(format: "%.0f", mainMultiplicand) + ":"
+        
+        // 2 x 2
+        if (mainMultiplier >= 10 && mainMultiplicand >= 10) {
+            let subMultiplier = floor(mainMultiplier / 10) * 10
+            let subMultiplicand = floor(mainMultiplicand / 10) * 10
+            
+            let subMultiplierRem = mainMultiplier - subMultiplier
+            let subMultiplicandRem = mainMultiplicand - subMultiplicand
+            
+            subExp1.text = String(format: "%.0f", subMultiplier) + " X " + String(format: "%.0f", subMultiplicand)
+            subExp2.text = String(format: "%.0f", subMultiplier) + " X " + String(format: "%.0f", subMultiplicandRem)
+            subExp3.text = String(format: "%.0f", subMultiplierRem) + " X " + String(format: "%.0f", subMultiplicand)
+            subExp4.text = String(format: "%.0f", subMultiplierRem) + " X " + String(format: "%.0f", subMultiplicandRem)
+            
+            let runningSum1 = subMultiplier * subMultiplicand
+            let runningSum2 = subMultiplier * subMultiplicandRem
+            let runningSum3 = subMultiplierRem * subMultiplicand
+            let runningSum4 = subMultiplierRem * subMultiplicandRem
+            
+            runningSum1Lbl.text = String(format: "%.0f", runningSum1)
+            runningSum2Lbl.text = String(format: "%.0f", runningSum1) + " + " + String(format: "%.0f", runningSum2) + " = " + String(format: "%.0f", runningSum1 + runningSum2)
+            runningSum3Lbl.text = String(format: "%.0f", runningSum1 + runningSum2) + " + " + String(format: "%.0f", runningSum3) + " = " + String(format: "%.0f", runningSum1 + runningSum2 + runningSum3)
+            runningSum4Lbl.text = String(format: "%.0f", runningSum1 + runningSum2 + runningSum3) + " + " + String(format: "%.0f", runningSum4) + " = " + String(format: "%.0f", runningSum1 + runningSum2 + runningSum3 + runningSum4)
+        }
+        // 2 x 1
+        else if (mainMultiplier >= 10) {
+            let subMultiplier = floor(mainMultiplier / 10) * 10
+            let subMultiplierRem = mainMultiplier - subMultiplier
+            
+            // handle mainMultiplicand directly as it is < 10
+            subExp1.text = String(format: "%.0f", subMultiplier) + " X " + String(format: "%.0f", mainMultiplicand)
+            subExp2.text = String(format: "%.0f", subMultiplierRem) + " X " + String(format: "%.0f", mainMultiplicand)
+            
+            let runningSum1 = subMultiplier * mainMultiplicand
+            let runningSum2 = subMultiplierRem * mainMultiplicand
+            
+            runningSum1Lbl.text = String(format: "%.0f", runningSum1)
+            runningSum2Lbl.text = String(format: "%.0f", runningSum1) + " + " + String(format: "%.0f", runningSum2) + " = " + String(format: "%.0f", runningSum1 + runningSum2)
+        }
+        // 1 x 2
+        else {
+            let subMultiplicand = floor(mainMultiplicand / 10) * 10
+            let subMultiplicandRem = mainMultiplicand - subMultiplicand
+            
+            // handle mainMultiplier directly as it is < 10
+            subExp1.text = String(format: "%.0f", mainMultiplier) + " X " + String(format: "%.0f", subMultiplicand)
+            subExp2.text = String(format: "%.0f", mainMultiplier) + " X " + String(format: "%.0f", subMultiplicandRem)
+            
+            let runningSum1 = mainMultiplier * subMultiplicand
+            let runningSum2 = mainMultiplier * subMultiplicandRem
+            
+            runningSum1Lbl.text = String(format: "%.0f", runningSum1)
+            runningSum2Lbl.text = String(format: "%.0f", runningSum1) + " + " + String(format: "%.0f", runningSum2) + " = " + String(format: "%.0f", runningSum1 + runningSum2)
+        }
     }
     
 
