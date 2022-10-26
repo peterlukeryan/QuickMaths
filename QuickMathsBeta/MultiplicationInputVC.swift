1d39ad28d43d3ba0b5dc20a0e4d257d23a90ad39
Vanessa Z <vanlouisezam@gmail.com>
2022-10-25 23:55:11 -0500

    Added some outlets for Multiplication Input Screen

--- QuickMathsBeta/MultiplicationInputVC.swift
+++ QuickMathsBeta/MultiplicationInputVC.swift
@@ -9,15 +9,27 @@ import UIKit
 
 class MultiplicationInputVC: UIViewController {
 
+    @IBOutlet weak var firstNumber: UITextField!
+    
+    @IBOutlet weak var secondNumber: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func showBtn(_ sender: Any) {
+        // perform input validation -- length of input must be less than 99
+        if (Int(firstNumber.text!)! > 99 || Int(firstNumber.text!)! > 99) {
+            // make error pop up asking user to enter two 2-digit numbers
+        }
+        // else allow segue
+        // transfer number data to the other screen -- probably using prepare?
+    }
 
     /*
-    // MARK: - Navigation
+     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
