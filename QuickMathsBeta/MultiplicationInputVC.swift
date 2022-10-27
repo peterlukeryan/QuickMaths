933099cdce05fbfbe8a6de94543318282456f19c
Vanessa Z <vanlouisezam@gmail.com>
2022-10-26 22:45:57 -0500

    test commit

--- QuickMathsBeta/MultiplicationInputVC.swift
+++ QuickMathsBeta/MultiplicationInputVC.swift
@@ -45,7 +45,6 @@ class MultiplicationInputVC: UIViewController {
         }
     }
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         if segue.identifier == "multi_seg" {
