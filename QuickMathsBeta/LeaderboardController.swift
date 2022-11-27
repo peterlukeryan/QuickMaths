f3d2acb373098676c967e3bcd406ca09afda632e
Vanessa Z <vanlouisezam@gmail.com>
2022-11-26 20:13:03 -0600

    added some tableView return statements in Leaderboard VC

--- QuickMathsBeta/LeaderboardController.swift
+++ QuickMathsBeta/LeaderboardController.swift
@@ -37,13 +37,11 @@ class LeaderboardController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return topScores.count
     }
 
     /*
