5daaa156b73e542679f04e34aa1e38a7e18ec061
Vanessa Z <vanlouisezam@gmail.com>
2022-11-26 20:02:36 -0600

    added data struct to leaderboard VC

--- QuickMathsBeta/LeaderboardController.swift
+++ QuickMathsBeta/LeaderboardController.swift
@@ -7,6 +7,21 @@
 
 import UIKit
 
+struct ScoreItem: Codable {
+    init() {
+        accuracy = 0
+        time = "00:00"
+        timestamp = "MM/DD/YYYY"
+    }
+    var accuracy: Int
+    var time: String
+    var timestamp: String
+}
+
+// How can we append data to this from another VC?
+// How do we keep the array populated after user navigates off this VC?
+var topScores: Array<ScoreItem> = []
+
 class LeaderboardController: UITableViewController {
 
     override func viewDidLoad() {
@@ -45,33 +60,6 @@ class LeaderboardController: UITableViewController {
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
         return true
     }
     */
